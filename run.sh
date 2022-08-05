1bae56c646e6556c45fa8c1725964f092c3520eb
Public <slpublic@Sectorlabss-Mac-mini.local>
2022-08-05 14:07:42 +0300

    changes run

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-gcc /app/backup.c -o /app/backup
-# run backup
-python3 /app/app.py /app/file
+if gcc /app/backup.c -o /app/backup; then
+  /app/backup $1 $2
+  python3 /app/app.py /app/file
+else
+  echo 'error at startup'
+fi
