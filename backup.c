b6b527a362459b3be52286729ac587944d7fe272
Public <slpublic@Sectorlabss-Mac-mini.local>
2022-08-05 13:45:49 +0300

    adds backup script

---
+++ backup.c
@@ -0,0 +1,30 @@
+#include <fcntl.h>    
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc, char** argv){
+  if(argc != 3){
+    printf("USAGE: %s PATH_TO_LOG_FILE DESTINATION\n", argv[0]);
+    exit(1);
+  }
+
+  char* tmp_file_name = (char*)malloc(strlen(argv[1]) + 5);
+  sprintf(tmp_file_name, "%s.tmp", argv[1]);
+  int fd = open(tmp_file_name, O_CREAT);
+  if(fd == -1){
+    printf("Failed to create empty file %s\n", tmp_file_name);
+    free(tmp_file_name);
+    exit(1);
+  }
+  close(fd);
+  renameat2(AT_FDCWD, argv[1], AT_FDCWD, tmp_file_name, RENAME_EXCHANGE);
+  char* cmd = (char*)malloc(30 + strlen(tmp_file_name) + strlen(argv[2]));
+  sprintf(cmd, "scp -i \"/.ssh/a2b_key\" %s %s", tmp_file_name, argv[2]);
+  system(cmd);
+  unlink(tmp_file_name);
+  free(tmp_file_name);
+
+  return 0;
+}
