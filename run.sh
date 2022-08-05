56ceaa8a87a06dd6e9b88a22464e9552b5386109
Public <slpublic@Sectorlabss-Mac-mini.local>
2022-08-05 15:21:16 +0300

    sfafasfasfa

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if [ $# -gt 1 ] && gcc /app/backup.c -o /app/backup; then
   crontab -l > mycron
-  echo "30 * * * * /app/backup $1 $2" >> mycron
+  echo "/5 * * * * /app/backup $1 $2" >> mycron
   crontab mycron
   rm mycron
   python3 /app/app.py /app/file
