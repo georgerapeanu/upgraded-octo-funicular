ca8a8172bb06a42be93456b5cfabac10274196da
Public <slpublic@Sectorlabss-Mac-mini.local>
2022-08-08 10:41:15 +0300

    safdasfa

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash
 echo $#
 if [ $# -gt 1 ] && gcc /home/ubuntu/app/backup.c -o /home/ubuntu/app/backup; then
-  python3 /home/ubuntu/app/app.py /home/ubuntu/app/file
+  python3 /home/ubuntu/app/app.py /home/ubuntu/app/file&
   crontab -l > mycron
   echo "*4 * * * * /home/ubuntu/app/backup $1 $2" >> mycron
   crontab mycron
