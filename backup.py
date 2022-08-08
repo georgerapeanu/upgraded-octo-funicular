857dfde15a46ab0a604b2a8649676d3009b2449b
Public <slpublic@Sectorlabss-Mac-mini.local>
2022-08-08 11:49:41 +0300

    sfasfsaf

---
+++ backup.py
@@ -0,0 +1,33 @@
+from os import unlink
+from pydoc import cli
+import sys
+from datetime import datetime
+from threading import local
+import paramiko
+import scp
+import renameat2
+
+def createSSHClient(server, user):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, user)
+    return client
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print(f'USAGE: {sys.argv[0]} path_to_file host destination_path')
+    sys.exit(1)
+  
+  localpath = sys.argv[1]
+  host = sys.argv[2]
+  destinationpath = sys.argv[3]
+  tmppath = "%s.tmp" % localpath
+
+  ssh = createSSHClient(host, "ubuntu")
+  f = open(tmppath, "w")
+  f.close()
+  renameat2.exchange(tmppath, localpath)
+  scp.put(ssh.get_transport(), tmppath, destinationpath)
+  ssh.close()
+  unlink(tmppath)
--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo $#
-if [ $# -gt 1 ] && gcc /home/ubuntu/app/backup.c -o /home/ubuntu/app/backup; then
-  python3 /home/ubuntu/app/app.py /home/ubuntu/app/file&
+if [ $# -gt 1 ] ; then
+  touch /home/ubuntu/app/logs
+  ln -s /home/ubuntu/app/logs_link
   crontab -l > mycron
-  echo "*4 * * * * /home/ubuntu/app/backup $1 $2" >> mycron
+  echo "*4 * * * * python3 /home/ubuntu/app/backup.py $1 $2 $3" >> mycron
   crontab mycron
   rm mycron
+  python3 /home/ubuntu/app/app.py /home/ubuntu/app/logs_link&
 else
   echo 'error at startup'
 fi
