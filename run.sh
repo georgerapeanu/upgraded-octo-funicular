c23e3cee2747ccab6b4c61344cac2fb88a4320be
Public <slpublic@Sectorlabss-Mac-mini.local>
2022-08-08 09:55:03 +0300

    sfasfgas

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 if [ $# -gt 1 ] && gcc /home/ubuntu/app/backup.c -o /home/ubuntu/app/backup; then
+  python3 /home/ubuntu/app/app.py /home/ubuntu/app/file
   crontab -l > mycron
   echo "*4 * * * * /home/ubuntu/app/backup $1 $2" > mycron
   crontab mycron
   rm mycron
-  python3 /home/ubuntu/app/app.py /home/ubuntu/app/file
 else
   echo 'error at startup'
 fi
